45498a0f509c7176d85b6dc1d2faaf915f57d80d
Julian Duque <julianduquej@gmail.com>
2012-12-11 20:42:15 -0500

    Remove IP address in socket.io connection

--- public/javascripts/pulse.js
+++ public/javascripts/pulse.js
@@ -1,5 +1,5 @@
 (function() {
-  var socket = io.connect('http://192.168.1.40:3000');
+  var socket = io.connect('http://localhost:3000');
 
   socket.on('sensor', function(sensor) {
     $('#heart .value').html(sensor.heartRate);
